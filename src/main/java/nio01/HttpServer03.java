42c35eb7441e97875cc2433ca43ec3327f882af9
J.Easter <295140325@qq.com>
2021-05-23 20:56:33 +0800

    create HttpServer03

---
+++ src/main/java/nio01/HttpServer03.java
@@ -0,0 +1,43 @@
+package nio01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executors;
+
+public class HttpServer03 {
+
+    public static void main(String[] args) throws IOException {
+        var executorService = Executors.newFixedThreadPool(40);
+        var serverSocket = new ServerSocket(8083);
+        while (true) {
+            try {
+                System.out.println("before accept");
+                var socket = serverSocket.accept();
+                System.out.println("after accept");
+                executorService.execute(()-> service(socket));
+                System.out.println("after service socket");
+            } catch (IOException aException) {
+                aException.printStackTrace();
+            }
+        }
+    }
+
+    private static void service(Socket aSocket) {
+        try {
+            Thread.sleep(20);
+            var printWriter = new PrintWriter(aSocket.getOutputStream(), true);
+            printWriter.println("HTTP/1.1 200 OK");
+            printWriter.println("Content-Type:text/html;charset=utf-8");
+            String body = "<h2>hello world</h2>";
+            printWriter.println("Content-Length: " + body.getBytes().length);
+            printWriter.println();
+            printWriter.write(body);
+            printWriter.close();
+            aSocket.close();
+        } catch (IOException | InterruptedException aException) {
+            aException.printStackTrace();
+        }
+    }
+}
