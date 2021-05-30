052bcf687b323c088b304fc73e8b7ff386bc1a80
J.Easter <295140325@qq.com>
2021-05-30 12:57:15 +0800

    print sth. when receive requests

--- src/main/java/nio02/DiscardServerHandler.java
+++ src/main/java/nio02/DiscardServerHandler.java
@@ -3,6 +3,7 @@ package nio02;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
 import io.netty.util.ReferenceCountUtil;
 
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
@@ -10,7 +11,18 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 
-        ((ByteBuf) msg).release();
+        var in = (ByteBuf) msg;
+        try {
+//            while (in.isReadable()) {
+//                System.out.println((char) in.readByte());
+//                System.out.flush();
+//            }
+            System.out.println(in.toString(CharsetUtil.US_ASCII));
+        } finally {
+            {
+                ReferenceCountUtil.release(msg);
+            }
+        }
     }
 
     @Override
